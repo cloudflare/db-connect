6c6cde4543a387d8137bd684852d93aa49e22a9e
Ashcon Partovi <ashcon@partovi.net>
2019-11-18 15:54:42 -0800

    Simplify and speed up the example worker

--- example/index.template.js
+++ example/index.template.js
@@ -1,68 +1,69 @@
 import { DbConnect } from '@cloudflare/db-connect'
+import { UAParser } from 'ua-parser-js'
 
 addEventListener('fetch', event => {
-  var cf = event.request.cf
-  if(!cf) {
-    console.warn('This quickstart works best when deployed to workers.dev or your own zone!')
-    cf = {}
-  }
-  event.respondWith(handleSql(
-    cf.colo || 'Unknown',
-    cf.country || 'Unknown'))
+  const colo = (event.request.cf || { colo: 'Unknown' }).colo
+  const headers = event.request.headers
+  const ray = headers.get('Cf-ray')
+  const agent = new UAParser(headers.get('User-agent'))
+
+  var device = agent.getDevice()
+  if(!device.vendor) device.vendor = agent.getOS().name
+  if(!device.model) device.model = agent.getBrowser().name
+
+  event.respondWith(handleSql(ray, colo, `${device.vendor} ${device.model}`.trim()))
 })
 
 const db = $CODE
 
-async function handleSql(colo, country) {
-
-  const ping = await db.ping()
-  if(!ping.ok) {
-    return ping
-  }
-
-  const table = await db.submit({
-    statement: 'CREATE TABLE IF NOT EXISTS quickstart ' +
-               '(colo TEXT, country TEXT, views INTEGER, ' +
-               'PRIMARY KEY (colo, country))',
-    mode: 'exec' })
-  if(!table.ok) {
-    return table
-  }
+async function handleSql(ray, colo, device) {
 
   const insert = await db.submit({
-    statement: 'INSERT OR IGNORE INTO quickstart VALUES (?, ?, ?)',
     mode: 'exec',
-    arguments: [ colo, country, 0 ] })
+    isolation: 'serializable',
+    arguments: [ ray, colo, device ],
+    cacheTtl: -1,
+    statement: `
+      CREATE TABLE IF NOT EXISTS quickstart
+        (ray TEXT, colo TEXT, device TEXT);
+      INSERT OR IGNORE INTO quickstart VALUES (?, ?, ?);`})
+
   if(!insert.ok) {
     return insert
   }
 
-  const update = await db.submit({
-    statement: 'UPDATE quickstart SET views = views + 1 ' +
-               'WHERE colo = ? AND country = ?',
-    mode: 'exec',
-    isolation: 'serializable',
-    arguments: [ colo, country ] })
-  if(!update.ok) {
-    return update
+  const [queryC, queryD] = await Promise.all([
+    db.submit({
+      mode: 'query',
+      cacheTtl: -1,
+      statement: `
+        SELECT colo, COUNT(*) AS views FROM quickstart
+          GROUP BY colo ORDER BY views DESC;`}),
+    db.submit({
+      mode: 'query',
+      cacheTtl: -1,
+      statement: `
+        SELECT device, COUNT(*) AS views FROM quickstart
+          GROUP BY device ORDER BY views DESC;`})
+  ])
+
+  for(const query of [queryC, queryD]) {
+    if(!query.ok) {
+      return query
+    }
   }
 
-  const queryTotal = await db.submit({
-    statement: 'SELECT SUM(views) AS views FROM quickstart' })
-  if(!queryTotal.ok) {
-    return queryTotal
-  }
+  const colos = (await queryC.json())
+    .map(row => `\t${row.colo}: ${row.views}`)
+    .join('\n')
 
-  const queryColo = await db.submit({
-    statement: 'SELECT colo, SUM(views) AS views FROM quickstart GROUP BY colo' })
-  if(!queryColo.ok) {
-    return queryColo
-  }
+  const devices = (await queryD.json())
+    .map(row => `\t${row.device}: ${row.views}`)
+    .join('\n')
 
-  return new Response(`Quickstart SQL Counter:
-    Number of Unique Views: ${(await queryTotal.json())[0].views}
-    Number of Unique Views by Colo:
-    ${(await queryColo.json())
-      .sort((a, b) => b.views - a.views)
-      .map(c => `\t${c.colo}: ${c.views}\n`)} `)
+  return new Response(`
+  === Quickstart for db-connect ===
+    Number of Views by Colo:\n${colos}
+    Number of Views by Device:\n${devices}
+  `)
 }
